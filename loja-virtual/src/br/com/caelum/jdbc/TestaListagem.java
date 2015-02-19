e577f5fbf949126b382cd4e7bd398e95e0a43e03
rogeriop <rogeriop@Luana>
2015-02-19 16:04:27 -0300

    Commit inicial

---
+++ loja-virtual/src/br/com/caelum/jdbc/TestaListagem.java
@@ -0,0 +1,30 @@
+package br.com.caelum.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
+		Statement statement = connection.createStatement();
+		boolean resultado = statement.execute("SELECT * FROM PRODUTO");
+		ResultSet resultSet = statement.getResultSet();
+		
+		int id = 0;
+		String nome = "";
+		String descricao = "";
+		
+		while (resultSet.next()) {
+			id = resultSet.getInt("id");
+			nome = resultSet.getString("nome");
+			descricao = resultSet.getString("descricao");
+			System.out.println(id + " " + nome + " " + descricao);
+		}
+		
+		resultSet.close();
+		statement.close();
+	}
+}
