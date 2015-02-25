36e080472ba90175bf3b66bfc0d5040c506706d6
rogeriop <rogeriop@Luana>
2015-02-25 16:31:48 -0300

    fim 06 Isolando o acesso aos dados e o DAO II

--- loja-virtual/src/br/com/caelum/jdbc/TestaProduto.java
+++ loja-virtual/src/br/com/caelum/jdbc/TestaProduto.java
@@ -2,13 +2,22 @@ package br.com.caelum.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public class TestaProduto {
-	public static void main(String[] args) throws SQLException {
-		Produto produto = new Produto(null, null);
-		try(Connection con = new ConnectionPool().getConnection()) {
-			
-		}
-		
-	}
+    public static void main(String[] args) throws SQLException {
+        Produto mesa = new Produto("Mesa Vermelha", "Mesa com 4 pés");
+
+        try (Connection con = new ConnectionPool().getConnection()) {
+            ProdutosDAO dao = new ProdutosDAO(con);
+            dao.salva(mesa);
+
+            List<Produto> produtos = dao.lista();
+            for (Produto produto : produtos) {
+                System.out.println(produto.id + " ; " + produto.nome + " ; " + produto.descricao);
+            }
+        }
+
+    }
+
 }
